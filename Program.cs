bcc0b8af5e0cf406ea7b0092729069856a5eb878
Joey Madronero <jmadronero@AQUARIUS.CORP>
2023-10-19 01:20:54 +0800

    Second Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue = Math.Max(firstValue,secondValue);
+// Console.WriteLine(largerValue);
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total > 14) {
+    Console.WriteLine("You win!");
+}
+
+if (total < 15) {
+    Console.WriteLine("You Lose!");
+}
+
+
+string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if(message.Contains("fox")) {
+    Console.WriteLine("What does the fox say?");
+}
